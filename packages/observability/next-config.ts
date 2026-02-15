d636aad31cc437e237c43064eed079b939ddc190
Eric <eric@spiritsdontfly.com>
2026-02-15 15:09:10 +0100

    chore: clean up turbo.json and enhance Sentry configuration with webpack tree-shaking

--- packages/observability/next-config.ts
+++ packages/observability/next-config.ts
@@ -25,16 +25,19 @@ export const sentryConfig: Parameters<typeof withSentryConfig>[1] = {
    */
   tunnelRoute: "/monitoring",
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
   /*
-   * Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+   * Automatically tree-shake Sentry logger statements to reduce bundle size,
+   * and enable automatic instrumentation of Vercel Cron Monitors.
    * See the following for more information:
    * https://docs.sentry.io/product/crons/
    * https://vercel.com/docs/cron-jobs
    */
-  automaticVercelMonitors: true,
+  webpack: {
+    treeshake: {
+      removeDebugLogging: true,
+    },
+    automaticVercelMonitors: true,
+  },
 };
 
 export const withSentry = (sourceConfig: object): object => {
